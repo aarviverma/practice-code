7af1e1e7175be4c367866b29af04b069b260c100
aarviverma <aarvi.verma.nnn@gmail.com>
2025-10-24 16:05:03 +0530

    Add files via upload

---
+++ printing_variables.py
@@ -0,0 +1,26 @@
+name = "Aarti"
+age = 25
+gender = "Female"
+
+# print("Name:", name)
+# print("Age:", age)
+# print("gender:", gender)
+
+# # Printing multiple variables in a single line
+# print("Name:", name, ", Age:", age, "gender:", gender)
+
+# # Printing the sum of two numeric variables
+# num1 = 15
+# num2 = 30
+# print("Sum of num1 and num2:", num1 + num2)
+
+# Method 1
+# print("My name is", name)
+# print("My age is", age)
+# print("My gender is", gender)
+# print(name, age, gender)
+
+# Method 2 (f strings)
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My name is {name}, my age is {age} and my gender is {gender}")
